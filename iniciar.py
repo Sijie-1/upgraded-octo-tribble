539fbe02237dedeb9088217c815a63b6f81b9e81
SijieCh <67526497+Sijie-1@users.noreply.github.com>
2025-09-28 22:43:59 +0000

    Agregar todo

---
+++ iniciar.py
@@ -0,0 +1,39 @@
+import subprocess
+import time
+import os
+
+# Directorios
+server_dir = "/workspaces/upgraded-octo-tribble/server"
+playit_dir = "/workspaces/upgraded-octo-tribble/playit"
+
+# Iniciar playit
+print("Iniciando Playit...")
+playit_process = subprocess.Popen(
+    [os.path.join(playit_dir, "playit")],
+    cwd=playit_dir,
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL
+)
+
+time.sleep(5)
+
+# Iniciar el sv de mc
+print("Iniciando servidor de Minecraft...")
+server_process = subprocess.Popen(
+    [
+        "java",
+        "-Xms14G",
+        "-Xmx14G",
+        "-jar",
+        "/workspaces/upgraded-octo-tribble/server/java-execute.jar"
+    ],
+    cwd=server_dir
+)
+
+try:
+    server_process.wait()
+    playit_process.wait()
+except KeyboardInterrupt:
+    print("\nDeteniendo procesos...")
+    server_process.terminate()
+    playit_process.terminate()
